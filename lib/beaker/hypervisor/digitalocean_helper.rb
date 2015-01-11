e7e11281f4189cccbd7e183872369779eeebc2cc
Peter Souter <peter.souter@puppetlabs.com>
2015-01-11 10:13:10 -0800

    Digital ocean spike

---
+++ lib/beaker/hypervisor/digitalocean_helper.rb
@@ -0,0 +1,12 @@
+require 'barge'
+require 'json'
+
+module Beaker
+  #Beaker helper module for doing API level Google Compute Engine interaction.
+  class DigitalOceanHelper
+
+    class DigitalOceanError < StandardError
+    end
+
+  end
+end
